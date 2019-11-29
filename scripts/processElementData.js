98e99d25b62ace46571c587aaef6052942fc38d0
KalebLight <kaleb.luiz00@gmail.com>
2019-11-28 23:13:41 -0300

    Adicionando a publicação

---
+++ scripts/processElementData.js
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_n9a5AQ-TEeqSJ8cDYfqRQQActivity"]="./../../lean_software_development_lsd/deliveryprocesses/resources/LSD_971AED54_971aed54_Activity.jpeg"
+contentPage.processPage.elementUrls["_UBrhYBFWEeqLdaj08i2c-Q"] = ["Buscar a perfeicao","lean_software_development_lsd/deliveryprocesses/buscar_a_perfeicao_1BB11A0.html"];
+contentPage.processPage.elementUrls["_1NrOIA-TEeqSJ8cDYfqRQQ"] = ["Especificar Valor","lean_software_development_lsd/deliveryprocesses/especificar_valor_F0C7F3D7.html"];
+contentPage.processPage.elementUrls["_Tk5ewBFWEeqLdaj08i2c-Q"] = ["Produção solicitada","lean_software_development_lsd/deliveryprocesses/producao_solicitada_EE3DEE66.html"];
+contentPage.processPage.elementUrls["_n9a5AQ-TEeqSJ8cDYfqRQQ"] = ["LSD","lean_software_development_lsd/deliveryprocesses/LSD_971AED54.html"];
+contentPage.processPage.elementUrls["__s67sBB5Eeq2H59o8D8qTQ"] = ["Criar mapa de tarefas","lean_software_development_lsd/deliveryprocesses/criar_mapa_de_tarefas_5398B95C.html"];
+contentPage.processPage.elementUrls["_SiNSsBFWEeqLdaj08i2c-Q"] = ["Produzir Continuamente","lean_software_development_lsd/deliveryprocesses/produzir_continuamente_6511CFFE.html"];
